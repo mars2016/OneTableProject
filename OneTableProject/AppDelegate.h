c2b366df03f6842f34241ac877bad10a0fb422ae
rqing <>
2019-07-25 16:45:44 +0800

    带分区的UITableView
    Signed-off-by: rqing <>

---
+++ OneTableProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneTableProject
+//
+//  Created by user on 2019/7/25.
+//  Copyright © 2019 rxgzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneTableProject/ViewController.m
@@ -0,0 +1,105 @@
+//
+//  ViewController.m
+//  OneTableProject
+//
+//  Created by user on 2019/7/25.
+//  Copyright © 2019 rxgzs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define kTitle @"title"
+#define xList @"list"
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    NSArray *goodsList;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //1.添加tableview
+    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+    
+    //2.初始化数据
+    goodsList = @[
+                  @{kTitle : @"精选特卖",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤", @"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle : @"饭后(含有茶点)",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle : @"茶点(含有茶点)",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle : @"素材水果拼盘",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤",]
+                    },
+                  @{kTitle : @"水果拼盘生鲜果",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤",]
+                    },
+                  @{kTitle : @"拼盘",
+                    xList : @[@"甜点组合"]
+                    },
+                  @{kTitle : @"烤鱼盘",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle : @"饮料",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤",@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle: @"小吃",
+                    xList : @[@"甜点组合", @"毛肚"]
+                    },
+                  @{kTitle : @"作料",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  @{kTitle : @"主食",
+                    xList : @[@"甜点组合", @"毛肚", @"菌汤"]
+                    },
+                  ];
+}
+
+//分区
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
+    return goodsList.count;
+}
+
+//行数
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    //1.获取session组的title
+    NSDictionary *dic = goodsList[section];
+    
+    //2.获取title中xlist
+    NSArray *list = dic[xList];
+    
+    return list.count;
+}
+
+//显示行内容
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    
+    //1.取出session 中row行文字数据
+    NSDictionary *dic = goodsList[indexPath.section];
+    NSArray *list = dic[xList];
+    
+    NSString *text = list[indexPath.row];
+    //2.显示数据
+    cell.textLabel.text = text;
+    return cell;
+}
+
+//显示头部标题
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    
+    NSDictionary *dic = goodsList[section];
+    return dic[kTitle];
+}
+@end
